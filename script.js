b3b173831f992e76f71f0eecfebb39a118f0a872
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-04-08 20:51:19 -0300

    Alterando a lógica dos IFs, e IF ELSEs.

--- script.js
+++ script.js
@@ -17,19 +17,19 @@ function verificar() {
         if(sexo[0].checked) {
             genero = 'Homem'
             
-            if(idade >= 0 && idade < 5) {
-                imag.src = 'criança-menino.jpg'
-                res.innerHTML = `Criança de ${idade} anos.`
+            if(idade >= 0 && idade < 10) {
+                imag.src = 'imagens/criança-menino.jpg'
+                res.innerHTML = `Detectamos uma criança de ${idade} anos.`
             }
-            else if(idade < 12) {
+            else if(idade > 9 && idade < 15) {
                 imag.src = 'menino.jpg'
-                res.innerHTML = `Menino de ${idade} anos.`
+                res.innerHTML = `Detectamos um  de ${idade} anos.`
             }
-            else if(idade < 25) {
+            else if(idade > 14 && idade < 25) {
                 imag.src = 'jovem-homem.jpg'
                 res.innerHTML = `Jovem de ${idade} anos.`
             }
-            else if(idade < 50) {
+            else if(idade > 24 && idade < 60) {
                 imag.src = 'homem.jpg'
                 res.innerHTML = `${genero} de ${idade} anos.`
             }
@@ -43,19 +43,19 @@ function verificar() {
         if(sexo[1].checked) {
             genero = 'Mulher'
             
-            if(idade >= 0 && idade < 5) {
+            if(idade >= 0 && idade < 10) {
                 imag.src = 'criança-menina.jpg'
                 res.innerHTML = `Criança de ${idade} anos.`
             }
-            else if(idade < 12) {
+            else if(idade > 9 && idade < 15) {
                 imag.src = 'menina.jpg'
                 res.innerHTML = `Menina de ${idade} anos.`
             }
-            else if(idade < 25) {
+            else if(idade > 14 && idade < 25) {
                 imag.src = 'jovem-mulher.jpg'
                 res.innerHTML = `Jovem de ${idade} anos.`
             }
-            else if(idade < 50) {
+            else if(idade > 24 && idade < 60) {
                 imag.src = 'mulher (2).jpg'
                 res.innerHTML = `${genero} de ${idade} anos.`
             }
